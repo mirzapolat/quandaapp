853157a04ebfb45f5579c9387f26d73799d3e585
Mirza Polat <46076246+mirzapolat@users.noreply.github.com>
2023-12-27 23:46:13 +0100

    reset metro config

--- metro.config.js
+++ metro.config.js
@@ -4,20 +4,13 @@ module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
-
-  // Füge 'svg' zur Liste der unterstützten Dateierweiterungen hinzu
-  const updatedSourceExts = [...sourceExts, 'svg'];
-
-  // Füge 'db' zur Liste der unterstützten Dateierweiterungen hinzu
-  const updatedAssetExts = [...assetExts, 'db'];
-
   return {
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
-      assetExts: updatedAssetExts,
-      sourceExts: updatedSourceExts,
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
     },
   };
 })();
